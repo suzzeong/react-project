3e6a6dcb1a2a598bf9e9fb7eed78fe58795e2877
suzzeong <suzzeong@suz-MacBook-Pro.local>
2024-04-04 22:34:00 +0900

    [Feat] 과제 A. React 프로젝트에서 검색엔진 대응하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Helmet>
+				<title>나만의 React 앱</title>
+				<meta name="description" content="나만의 React 앱에 대한 설명" />
+				<meta property="og:title" content="나만의 React 앱" />
+				<meta
+					property="og:description"
+					content="이것은 나만의 React 앱입니다."
+				/>
+				<meta property="og:image" content="여기에 이미지 URL을 넣으세요" />
+				<meta property="og:url" content="https://www.yourdomain.com" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="나만의 React 앱" />
+				<meta
+					name="twitter:description"
+					content="이것은 나만의 React 앱입니다."
+				/>
+				<meta name="twitter:image" content="여기에 이미지 URL을 넣으세요" />
+			</Helmet>
+			{/* 애플리케이션의 나머지 부분 */}
+			이것은 나만의 React 앱입니다.
+		</div>
+	);
 }
 
 export default App;
